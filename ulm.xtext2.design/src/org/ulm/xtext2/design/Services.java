7d1966db477683f06e9ed6c67703abb58d0cfb9a
steve.hostettler <Steve.Hostettler@CHWS03042-01.wkfs-frc.local>
2018-07-24 19:43:59 +0200

    # WARNING: head commit changed in the meantime
    Add pruned folders

--- ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
+++ ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
@@ -1,29 +1,17 @@
 package org.ulm.xtext2.design;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.eclipse.emf.ecore.EObject;
-
-import org.ulm.metaulm.xtext.ulmDsl2.Entity;
-import org.ulm.metaulm.xtext.ulmDsl2.EntityFeatureType;
+import org.ulm.metaulm.xtext.ulmDsl2.AttributeFeatureType;
 import org.ulm.metaulm.xtext.ulmDsl2.Feature;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
-    
-    /**
-    * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
-    */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
-    }
-    
-    public Set<Feature> allEntityFeatureType(Entity entity) {
-    	
-    	return entity.getFeatures().stream().filter(e -> e.getFeatureType().getType().getT() instanceof EntityFeatureType).collect(Collectors.toSet());
-    }
+
+	public String getNameOrAlias(AttributeFeatureType attribute) {
+
+		Feature feature = (Feature) attribute.eContainer().eContainer().eContainer();
+		return feature.getAlias() != null ? feature.getAlias() : attribute.getAttributeType().getName();
+	}
+
 }
