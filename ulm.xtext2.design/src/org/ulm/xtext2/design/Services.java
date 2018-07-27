27bb0dad91bc4f27bd3c04f3345a167af1f697ca
Vanzeune, Laurent <laurent.vanzeune@wolterskluwer.com>
2018-07-27 12:43:42 +0200

    Adapted as per grammar changes

--- ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
+++ ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
@@ -1,5 +1,9 @@
 package org.ulm.xtext2.design;
 
+import org.ulm.metaulm.xtext.ulmDsl2.Attribute;
+import org.ulm.metaulm.xtext.ulmDsl2.AttributeDecimalType;
+import org.ulm.metaulm.xtext.ulmDsl2.AttributeStringType;
+import org.ulm.metaulm.xtext.ulmDsl2.AttributeType;
 import org.ulm.metaulm.xtext.ulmDsl2.AttributeFeatureType;
 import org.ulm.metaulm.xtext.ulmDsl2.Feature;
 
@@ -11,7 +15,17 @@ public class Services {
 	public String getNameOrAlias(AttributeFeatureType attribute) {
 
 		Feature feature = (Feature) attribute.eContainer().eContainer().eContainer();
-		return feature.getAlias() != null ? feature.getAlias() : attribute.getAttributeType().getName();
+		return feature.getName() != null ? feature.getName() : attribute.getAttribute().getName();
 	}
 
+	public String getAttributeTypeAsString(Attribute attribute) {
+
+		if (attribute.getType() instanceof AttributeStringType) {
+			return ((AttributeStringType) attribute.getType()).getName();
+		} else if (attribute.getType() instanceof AttributeDecimalType) {
+			return ((AttributeDecimalType) attribute.getType()).getName();
+		} else {
+			return ((AttributeType) attribute.getType()).getName();
+		}
+	}
 }
