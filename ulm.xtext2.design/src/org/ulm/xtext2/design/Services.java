21e09d8be5fdfa8f38e5e23f3a223f65eb4c0ec0
steve.hostettler <Steve.Hostettler@wkfs-frc.local>
2018-07-27 15:58:25 +0200

    fix design

--- ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
+++ ulm.xtext2.design/src/org/ulm/xtext2/design/Services.java
@@ -1,10 +1,8 @@
 package org.ulm.xtext2.design;
 
 import org.ulm.metaulm.xtext.ulmDsl2.Attribute;
-import org.ulm.metaulm.xtext.ulmDsl2.AttributeDecimalType;
-import org.ulm.metaulm.xtext.ulmDsl2.AttributeStringType;
-import org.ulm.metaulm.xtext.ulmDsl2.AttributeType;
 import org.ulm.metaulm.xtext.ulmDsl2.AttributeFeatureType;
+import org.ulm.metaulm.xtext.ulmDsl2.EntityFeatureType;
 import org.ulm.metaulm.xtext.ulmDsl2.Feature;
 
 /**
@@ -12,20 +10,34 @@ import org.ulm.metaulm.xtext.ulmDsl2.Feature;
  */
 public class Services {
 
-	public String getNameOrAlias(AttributeFeatureType attribute) {
+	public String getNameOrAlias(AttributeFeatureType attributefeatureType) {
+		Feature feature = (Feature) attributefeatureType.eContainer().eContainer();
 
-		Feature feature = (Feature) attribute.eContainer().eContainer().eContainer();
-		return feature.getName() != null ? feature.getName() : attribute.getAttribute().getName();
+		if (feature.getName() != null) {
+			return feature.getName();
+		} else {
+			return  attributefeatureType.getAttribute().getName();
+		}
+	}
+	
+	public String getNameOrAlias(EntityFeatureType entityfeatureType) {
+		Feature feature = (Feature) entityfeatureType.eContainer().eContainer();
+
+		if (feature.getName() != null) {
+			return feature.getName();
+		} else {
+			return  entityfeatureType.getEntity().getName();
+		}
 	}
 
 	public String getAttributeTypeAsString(Attribute attribute) {
-
-		if (attribute.getType() instanceof AttributeStringType) {
-			return ((AttributeStringType) attribute.getType()).getName();
-		} else if (attribute.getType() instanceof AttributeDecimalType) {
-			return ((AttributeDecimalType) attribute.getType()).getName();
-		} else {
-			return ((AttributeType) attribute.getType()).getName();
-		}
+		return "tutu";
+//		if (attribute.getType() instanceof AttributeStringType) {
+//			return ((AttributeStringType) attribute.getType()).getName();
+//		} else if (attribute.getType() instanceof AttributeDecimalType) {
+//			return ((AttributeDecimalType) attribute.getType()).getName();
+//		} else {
+//			return ((AttributeType) attribute.getType()).getName();
+//		}
 	}
 }
